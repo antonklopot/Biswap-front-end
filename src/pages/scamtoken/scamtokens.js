6fd1d0af108a554afdca890fe114783f107bdf4a
Anton Klopot <anton.klopot@outlook.com>
2021-12-16 00:47:31 +0800

    first commit

---
+++ src/pages/scamtoken/scamtokens.js
@@ -0,0 +1,13 @@
+import Footer from '../../components/common/footer';
+import data from '../../data/data.json';
+
+function ScamTokens () {
+    return (
+        <div>
+
+        </div>
+
+    )
+}
+
+export default ScamTokens();
